387a5f6cc37456e38aca1e4cea30de1230cc2edf
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-03 18:00:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,7 @@
 # or whitespace (' ').
 class EmailParser
 
-  attr_accessor :email_addresses
+  attr_accessor :EmailAddressParser
 
   def initialize(email_addresses)
     @email_addresses = email_addresses
